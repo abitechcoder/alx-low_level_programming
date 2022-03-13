e4219d1ea67065e0ae1203b784033f5e0a12769c
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-03-13 20:15:45 +0000

    Task done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
+ * Return - 1 for Success
+ *
+ */
+
+int main(void)
+{
+	write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}

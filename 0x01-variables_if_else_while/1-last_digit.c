49dd159d07a97e6992f5b458fe85ffab34fc5ebf
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-03-14 09:20:20 +0000

    task done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /* more headers goes there */
 
 /**
- * Main - print the last digit of the number stored in the variable n
+ * main - print the last digit of the number stored in the variable n
  *
  * Return: Always (0) for success
  * 

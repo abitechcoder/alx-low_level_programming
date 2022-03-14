4449a6f0a9582af6c067bcd9b470ab4a78c1ca32
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-03-14 09:17:36 +0000

    task done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,10 +24,10 @@ int main(void)
 		printf("and is greater than 5\n");
 	}else if (n % 10 == 0)
 	{
-		printf("and is 0");
+		printf("and is 0\n");
 	}else
 	{
-		printf("and is less than 6 and not 0");
+		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
 }

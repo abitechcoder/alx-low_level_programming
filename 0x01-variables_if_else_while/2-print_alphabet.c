edcf4a33344169c1fae9418091f23bb19b49006e
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-03-14 11:25:30 +0000

    Task done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}

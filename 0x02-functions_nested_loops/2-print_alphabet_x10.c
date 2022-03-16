fd237772fb281e68d09f9954ecf966d208a8b719
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-03-16 21:19:58 +0000

    Task done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
+		_putchar('\n');
+	}
+}

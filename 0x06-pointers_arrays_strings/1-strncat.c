b18e0521efd1febccdeccab03f6695cd20eaefd8
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-03-24 14:04:47 +0000

    Task done

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first parameter
+ * @s2: second parameter
+ *
+ * Return: less than, equal to or greater than 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = strcmp(s1, s2);
+	return (res);
+}

df8191f4aae4d45cfb3a5ed04f637ccbd92b1d4f
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-03-30 22:06:44 +0000

    Task done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string parameter
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints -putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char text[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

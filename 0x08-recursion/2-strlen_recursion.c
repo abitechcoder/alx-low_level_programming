6811c608169af7be346b43b41baf7eb6871ad57a
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-03-31 15:51:16 +0000

    Task done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns the lengthof a string
+ * @s: string parameter
+ * Return: length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}

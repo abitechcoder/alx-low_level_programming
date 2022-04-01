d973996cb4f0753423663dc7c9a810fda2d4e000
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-04-01 21:21:18 +0000

    Task done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments in argv array
+ * @argv: arguments array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		int sum = 0;
+		int i;
+
+		for (i = 1; i < argc; i++)
+		{
+			sum += 1;
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	printf("%d\n", 0);
+	return (0);
+}

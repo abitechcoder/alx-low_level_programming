0eac34009fe6f00fa13dac09599b4be95eaa736f
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-04-01 11:55:14 +0000

    Task done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,7 +14,7 @@ int is_prime_number(int n)
 	}
 	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
 	{
-		if(n == 2 || n == 3 || n == 5 || n == 7)
+		if (n == 2 || n == 3 || n == 5 || n == 7)
 		{
 			return (1);
 		}

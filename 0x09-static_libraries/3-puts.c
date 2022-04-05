865e1032f98718b7db24285e766d8edc3217be13
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-04-05 22:06:12 +0000

    task done

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
 /**
- * puts - print a string
+ *_puts - prints a string
  * @str: string to print
- * Description: print a string
+ *
+ * Description: prints a string
+ * On success: return the number of characters printed
  */
 
 void _puts(char *str)
 {
-	while (*str)
-	{
-		_putchar(*str++);
-	}
-	_putchar('\n');
+while (*str)
+_putchar(*str++);
+
+_putchar('\n');
 }
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,4 +1,15 @@
-int _abs(int n)
+#include "main.h"
+/**
+* _abs - prints the absolute value of a number
+*@i: number targeted
+*Return: returns 0
+*/
+int _abs(int i)
 {
-	return (1);
+if (i > 0)
+return (i);
+else if (i < 0)
+return (-i);
+else
+return (0);
 }

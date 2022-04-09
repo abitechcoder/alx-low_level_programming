5e641aef8be14175fe8fc45407eeb1ff4fd41b2c
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-04-09 17:40:02 +0000

    memory fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,19 +17,19 @@ char *_strdup(char *str)
 	}
 
 	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	s = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (s == NULL)
 	{
-
-		s = (char *)malloc(sizeof(char) * (i + 1));
-
-		if (s == NULL)
-		{
-			return (NULL);
-		}
-
-		for (j = 0; j <= i; j++)
-		{
-			s[j] = str[j];
-		}
+		return (NULL);
 	}
+
+	for (j = 0; j <= i; j++)
+	{
+		s[j] = str[j];
+	}
+
 	return (s);
 }

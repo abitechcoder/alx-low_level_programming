45cd357095309d69cea98d654ad78fabc6bd031c
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-04-09 17:22:09 +0000

    Task 2 done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,7 +9,9 @@
 char *_strdup(char *str)
 {
 	char *s;
-	unsigned int i, j;
+	unsigned int i, j, str_len;
+
+	str_len = 0;
 
 	if (str == NULL)
 	{
@@ -17,9 +19,11 @@ char *_strdup(char *str)
 	}
 
 	for (i = 0; str[i] != '\0'; i++)
-		;
+	{
+		str_len += 1;
+	}
 
-	s = (char *)malloc(sizeof(char) * (i + 1));
+	s = (char *)malloc(str_len);
 
 	if (s == NULL)
 	{

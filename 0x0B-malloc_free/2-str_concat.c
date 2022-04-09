e41514a805278c7b658b8944fb55405be5f191a8
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-04-09 21:15:47 +0000

    a quick fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,16 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	unsigned int i, j, size;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	size = strlen(s1) + strlen(s2) + 1;
 
 	str = malloc(size);
@@ -23,15 +33,32 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (i = 0; i < strlen(s1); i++)
+	if (strlen(s1) == 0)
 	{
-		str[i] = s1[i];
-
-		if (i == strlen(s1) - 1)
+		for (j = 0; j < strlen(s2); j++)
 		{
-			for (j = 0; j < strlen(s2); j++)
+			str[j] = s2[j];
+		}
+	}
+	else if (strlen(s2) == 0)
+	{
+		for (i = 0; i < strlen(s1); i++)
+		{
+			str[i] = s2[i];
+		}
+	}
+	else
+	{
+		for (i = 0; i < strlen(s1); i++)
+		{
+			str[i] = s1[i];
+
+			if (i == strlen(s1) - 1)
 			{
-				str[i + j + 1] = s2[j];
+				for (j = 0; j < strlen(s2); j++)
+				{
+					str[i + j + 1] = s2[j];
+				}
 			}
 		}
 	}

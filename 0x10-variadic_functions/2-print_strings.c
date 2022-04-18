8edbf70b76cbc6fda8bd23fde4ec778d34971f22
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-04-18 12:54:01 +0000

    Task 2 done

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - print strings separated by a separator
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+
+	va_list valist;
+
+	va_start(valist, n);
+
+	i = 0;
+
+	while (i < n)
+	{
+		str = va_arg(valist, char*);
+		printf("%s", str);
+		if (separator && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -3,3 +3,5 @@
 ## sum_them_all - returns the sum of all its parameters
 
 ## print_numbers - prints numbers separated by a separator parameter
+
+## print_strings - prints strings separated by a separator parameter

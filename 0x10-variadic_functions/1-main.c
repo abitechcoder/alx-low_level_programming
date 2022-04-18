8de154632e5822c729d054dcf91616cb10c32717
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-04-18 12:21:30 +0000

    added va_end to the end of my function

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stddef.h>
 
 /**
  * main - check the code
@@ -8,5 +9,8 @@
 int main(void)
 {
 	print_numbers(", ", 4, 0, 98, -1024, 402);
+	print_numbers(" ", 4, 0, 98, -1024, 402);
+	print_numbers("", 4, 0, 98, -1024, 402);
+	print_numbers(NULL, 4, 0, 98, -1024, 402);
 	return (0);
 }

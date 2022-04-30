763fed7f624447fee0b5404f32417f2b35e14225
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-04-30 17:21:50 +0000

    Task done

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * set_bit - function that sets the value of a bit to 1 at a given index
+ * @n: a pointer parameter to the original value
+ * @index: index value
+ * Return: 1 if it worked or -1 if an error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 16)
+	{
+		return (-1);
+	}
+
+	index = 1 << index;
+	*n = (*n | index);
+	return (1);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * clear_bit - function that sets the value of a bit to 0 at a given index
+ * @n: pointer parameter to the value
+ * @index: index parameter
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (*n == 0)
+		return (0);
+
+	if (index > 16)
+		return (-1);
+
+	index = 1 << index;
+	*n = (*n ^ index);
+	return (1);
+}

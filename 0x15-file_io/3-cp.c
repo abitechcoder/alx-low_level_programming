d3c7fc367073a6aff2d0b589e67255fa5a84f22a
Abiola Adeosun <abiolaolalekan39@gmail.com>
2022-05-04 21:48:49 +0000

    Task done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,12 +12,12 @@ void print_error(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
@@ -52,13 +52,13 @@ int main(int argc, char *argv[])
 	close_from = close(file_from);
 	if (close_from == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", close_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close_from);
 		exit(100);
 	}
 	close_to = close(file_to);
 	if (close_to == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", close_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", close_to);
 		exit(100);
 	}
 	return (1);
